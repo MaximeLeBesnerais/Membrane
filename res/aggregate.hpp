9458492a2fcd445c948c4a5021e9615e345fb433
Maxime <maxoulebesnerais@epitech.eu>
2025-03-01 23:14:59 +0800

    Remove obsolete HTML and header files for cleaner project structure

--- res/aggregate.hpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by maxime on 3/1/25.
-//
-
-#ifndef AGGREGATE_HPP
-    #define AGGREGATE_HPP
-    #include "headers/about.hpp"
-    #include "headers/index.hpp"
-#endif //AGGREGATE_HPP
