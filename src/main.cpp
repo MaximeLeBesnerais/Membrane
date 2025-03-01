491817ee2837235c285ad12ae66e1aa401a4e6cc
Maxime <maxoulebesnerais@epitech.eu>
2025-03-02 00:13:12 +0800

    Refactor main.cpp to replace aggregate.hpp with resource_init.hpp and initialize resources for improved clarity and structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <nlohmann/json.hpp>
 #include "Membrane_lib/Membrane.hpp"
-#include "aggregate.hpp"
+#include "resource_init.hpp"
 
 using json = nlohmann::json;
 
 
 int main() {
     Membrane app("Membrane App", "index.html", 800, 600, WEBVIEW_HINT_NONE);
-    app.add_vfs("index.html", res_index_html, res_index_html_len);
-    app.add_vfs("about.html", res_about_html, res_about_html_len);
-    app.add_vfs("style.css", res_style_css, res_style_css_len);
+    initialize_resources(app);
     app.run();
     return 0;
 }
