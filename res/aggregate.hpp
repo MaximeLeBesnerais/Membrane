ad10ada67b5e9264c1157c4ca66fa17e9c4d32c8
Maxime <maxoulebesnerais@epitech.eu>
2025-03-01 20:45:32 +0800

    Easier imports

---
+++ res/aggregate.hpp
@@ -0,0 +1,9 @@
+//
+// Created by maxime on 3/1/25.
+//
+
+#ifndef AGGREGATE_HPP
+    #define AGGREGATE_HPP
+    #include "headers/about.hpp"
+    #include "headers/index.hpp"
+#endif //AGGREGATE_HPP
