22188cd9351f2c476a1b161505ed607a3891e035
Maxime <maxoulebesnerais@epitech.eu>
2025-03-02 18:01:46 +0800

    Update Membrane app dimensions for improved user interface

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "resource_init.hpp"
 
 int main() {
-    Membrane app("Membrane App", "index.html", 800, 600, WEBVIEW_HINT_NONE);
+    Membrane app("Membrane", "index.html", 1200, 800, WEBVIEW_HINT_NONE);
     initialize_resources(app);
     app.run();
     return 0;
