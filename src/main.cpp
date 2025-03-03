02cf9ed7523bf1cb9ec2cd3353dd66e2597ad9ec
Maxime <maxoulebesnerais@epitech.eu>
2025-03-03 17:50:44 +0800

    Update Membrane instance parameters for SSH Key Gen application

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include "resource_init.hpp"
 
 int main() {
-    Membrane app("Membrane", "index.html", 1200, 800, WEBVIEW_HINT_NONE);
+    Membrane app("SSH Key Gen", "index.html", 620, 880, WEBVIEW_HINT_FIXED);
     initialize_resources(app);
+
     app.run();
     return 0;
 }
